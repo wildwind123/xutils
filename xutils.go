d5cd5667c977cc1be0e99f770d823140cfe8d8f7
wildwind123 <34130873+wildwind123@users.noreply.github.com>
2024-10-30 12:19:10 +0900

    Update xutils.go

--- xutils.go
+++ xutils.go
@@ -50,3 +50,10 @@ func StringToInt64(v string) (int64, error) {
 
 	return num, nil
 }
+
+func Bulk[T any](items []T, chunkSize int) (chunks [][]T) {
+	for chunkSize < len(items) {
+		items, chunks = items[chunkSize:], append(chunks, items[0:chunkSize:chunkSize])
+	}
+	return append(chunks, items)
+}
